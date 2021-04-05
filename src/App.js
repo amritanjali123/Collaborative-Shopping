b97853cbb32568976a094669b0b35fec6488c216
amritanjali123 <amritanjali918@gmail.com>
2021-04-05 17:26:47 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import Video from './Video'
+import Home from './Home'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+class App extends Component {
+	render() {
+		return (
+			<div>
+				<Router>
+					<Switch>
+						<Route path="/:url" component={Video} />
+						<Route path="/" exact component={Home} />
+						{/* default 
+						2 div cant be return  */}
+					</Switch>
+				</Router>
+			</div>
+		)
+	}
+}
+
+export default App;
---
+++ src/Home.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import { Input, Button, IconButton } from '@material-ui/core';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import "./Home.css"
+
+class Home extends Component {
+  	constructor (props) {
+		super(props)
+		this.state = {
+			url: ''
+		}
+	}
+
+	handleChange = (e) => this.setState({ url: e.target.value })
+
+	join = () => {
+		if (this.state.url !== "") {
+			var url = this.state.url.split("/")
+			window.location.href = `/${url[url.length-1]}`
+		} else {
+			var url = Math.random().toString(36).substring(2, 7)
+			window.location.href = `/${url}`
+		}
+	}
+
+	render() {
+		return (
+			<div style={{background: "#f4c2c2"}} className="container">
+				<div style={{fontSize: "14px", background: "#FFA62F", width: "10%", textAlign: "center", margin: "auto", marginBottom: "10px"}}>
+					 
+					<IconButton style={{color: "black"}} onClick={() => window.location.href="https://www.myntra.com/"}>
+						< img src={"/myntra.png"} style={{width: 200, height: 200}} />
+					</IconButton>
+				</div>
+				
+				<div>
+					<h1 style={{ fontSize: "45px" }}>Collaborative Shopping</h1>
+					<p style={{ fontWeight: "200" }}>We are here to give you real shoping experience virtually ~MYNTRA</p>
+				</div>
+
+				<div style={{
+					background: "white", width: "30%", height: "auto", padding: "20px", minWidth: "400px",
+					textAlign: "center", margin: "auto", marginTop: "100px"
+				}}>
+					<p style={{ margin: 0, fontWeight: "bold", paddingRight: "50px" }}>Start Discussion</p>
+					<Input placeholder="URL" onChange={e => this.handleChange(e)} />
+					<Button variant="contained" color="primary" onClick={this.join} style={{ margin: "20px" }}>Go</Button>
+				</div>
+			</div>
+		)
+	}
+}
+
+export default Home;
